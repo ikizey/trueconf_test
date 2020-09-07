fdae684d820852d23623adcd87455d99339ffaf8
ikizey <ikizey@gmail.com>
2020-09-07 13:32:40 +0300

    add user inserting @PUT "/users/add"

--- users.py
+++ users.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
@@ -15,7 +15,7 @@ class Users(Resource):
             return json.load(usersJSON)
 
 
-class User(Resource):
+class UserById(Resource):
     def get(self, id):
         return self._get_user_from_json(id)
 
@@ -25,8 +25,44 @@ class User(Resource):
             return users.get(id, {"message": "not exists"})
 
 
+class UserByName(Resource):
+    def put(self):
+        json = request.json
+        print(json.keys())
+        return self._add_user(json)
+
+    def _is_valid(self, post_json):
+        return True if list(post_json.keys()) == ['name'] else False
+
+    def _get_max_id(self):
+        return max(int(key) for key in Users().get().keys())
+
+    def _add_user(self, json):
+        if not self._is_valid(json):
+            return {"message": "use format: {'name': <name>}"}
+
+        id = self._get_max_id() + 1
+        user = {"id": id}
+        user.update(json)
+        print(id, user, json)
+        self._add_user_to_json_db(user, id)
+
+        return user
+
+    def _add_user_to_json_db(self, user, id):
+        with open("users_db.json", "r+") as jsonFile:
+            data = json.load(jsonFile)
+
+            data[id] = user
+
+            jsonFile.seek(0)
+            json.dump(data, jsonFile)
+            jsonFile.truncate()
+
+
 api.add_resource(Users, "/")
-api.add_resource(User, "/user/<id>")
+api.add_resource(UserById, "/user/<id>")
+api.add_resource(UserByName, "/users/add")
 
 if __name__ == "__main__":
     app.run(debug=True)
