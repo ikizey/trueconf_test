574be69fb3bb87d5083fb2b672b6fb3463d39e33
ikizey <ikizey@gmail.com>
2020-09-07 11:23:47 +0300

    add get user by id @GET "/user/<id>"

--- users.py
+++ users.py
@@ -1,5 +1,4 @@
 import json
-import pathlib
 from flask import Flask
 from flask_restful import Api, Resource
 
@@ -16,7 +15,18 @@ class Users(Resource):
             return json.load(usersJSON)
 
 
+class User(Resource):
+    def get(self, id):
+        return self._get_user_from_json(id)
+
+    def _get_user_from_json(self, id):
+        with open("users_db.json", "r") as usersJSON:
+            users = json.load(usersJSON)
+            return users.get(id, {"message": "not exists"})
+
+
 api.add_resource(Users, "/")
+api.add_resource(User, "/user/<id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
